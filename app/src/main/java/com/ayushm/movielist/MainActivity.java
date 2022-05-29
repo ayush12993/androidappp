8ed271710b606da1fe0c9b665289e11090caec8e
ayush12993 <ayush2202ksr@gmail.com>
2022-05-29 14:12:29 +0530

    task 2 90% complete, working on some changes

--- app/src/main/java/com/ayushm/movielist/MainActivity.java
+++ app/src/main/java/com/ayushm/movielist/MainActivity.java
@@ -1,11 +1,15 @@
 package com.ayushm.movielist;
 
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.ayushm.movielist.RequestManager.APIClient;
 import com.ayushm.movielist.RequestManager.APIInterface;
+import com.ayushm.movielist.adapter.MovieAdapter;
 import com.ayushm.movielist.model.MoviesList;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,11 +23,19 @@ public class MainActivity extends AppCompatActivity {
     TextView responseText;
     APIInterface apiInterface;
 
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        responseText = (TextView) findViewById(R.id.setTessst);
+        recyclerView = findViewById(R.id.moviesList_recyclerView);
+
+         listingData();
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(llm);
+    }
+
+    private void listingData() {
         apiInterface = APIClient.getClient().create(APIInterface.class);
 
 
@@ -35,116 +47,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<MoviesList>> call, Response<List<MoviesList>> response) {
 
-                      if(!response.isSuccessful()) {
-                          responseText.setText("Code: "+ response.code());
-                          return;
-                      }
-                          List<MoviesList> moviesLists = response.body();
-                            for (MoviesList moviesList : moviesLists){
-                                  String strmovieLists = "";
-                                  strmovieLists += "num" + moviesList.getNum() + "\n" ;
-                                  strmovieLists += "name" + moviesList.getName() + "\n" ;
-                                 responseText.append(strmovieLists);
+                if(!response.isSuccessful()) {
+                    responseText.setText("Code: "+ response.code());
+                    return;
+                }
 
-                      }
-//                String displayResponse = "";
-//
-//                MoviesList resource = response.body();
-//                Integer text = resource.getNum();
-//                Integer total = resource.getRating();
-//                Integer totalPages = resource.getStreamId();
-//                List<MoviesList> datumList = resource.data;
-//
-//                displayResponse += text + " Page\n" + total + " Total\n" + totalPages + " Total Pages\n";
-//
-//                for (MultipleResource.Datum datum : datumList) {
-//                    displayResponse += datum.id + " " + datum.name + " " + datum.pantoneValue + " " + datum.year + "\n";
-//                }
-//
-//                responseText.setText(displayResponse);
+                if (response.isSuccessful()){
+                    MovieAdapter movieAdapter = new MovieAdapter(response.body());
+                    recyclerView.setAdapter(movieAdapter);
+              return;
+                }
 
-            }
+//                List<MoviesList> moviesLists = response.body();
+//                for (MoviesList moviesList : moviesLists){
+//                    String strmovieLists = "";
+//                    strmovieLists += "num" + moviesList.getNum() + "\n" ;
+//                    strmovieLists += "name" + moviesList.getName() + "\n" ;
+//                    responseText.append(strmovieLists);
+
+                }
+
+
 
             @Override
             public void onFailure(Call<List<MoviesList>> call, Throwable t) {
                 call.cancel();
             }
         });
-
-//        /**
-//         Create new user
-//         **/
-//        User user = new User("morpheus", "leader");
-//        Call<User> call1 = apiInterface.createUser(user);
-//        call1.enqueue(new Callback<User>() {
-//            @Override
-//            public void onResponse(Call<User> call, Response<User> response) {
-//                User user1 = response.body();
-//
-//                Toast.makeText(getApplicationContext(), user1.name + " " + user1.job + " " + user1.id + " " + user1.createdAt, Toast.LENGTH_SHORT).show();
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<User> call, Throwable t) {
-//                call.cancel();
-//            }
-//        });
-//
-//        /**
-//         GET List Users
-//         **/
-//        Call<UserList> call2 = apiInterface.doGetUserList("2");
-//        call2.enqueue(new Callback<UserList>() {
-//            @Override
-//            public void onResponse(Call<UserList> call, Response<UserList> response) {
-//
-//                UserList userList = response.body();
-//                Integer text = userList.page;
-//                Integer total = userList.total;
-//                Integer totalPages = userList.totalPages;
-//                List<UserList.Datum> datumList = userList.data;
-//                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
-//
-//                for (UserList.Datum datum : datumList) {
-//                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
-//                }
-//
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<UserList> call, Throwable t) {
-//                call.cancel();
-//            }
-//        });
-//
-//
-//        /**
-//         POST name and job Url encoded.
-//         **/
-//        Call<UserList> call3 = apiInterface.doCreateUserWithField("morpheus","leader");
-//        call3.enqueue(new Callback<UserList>() {
-//            @Override
-//            public void onResponse(Call<UserList> call, Response<UserList> response) {
-//                UserList userList = response.body();
-//                Integer text = userList.page;
-//                Integer total = userList.total;
-//                Integer totalPages = userList.totalPages;
-//                List<UserList.Datum> datumList = userList.data;
-//                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
-//
-//                for (UserList.Datum datum : datumList) {
-//                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<UserList> call, Throwable t) {
-//                call.cancel();
-//            }
-//        });
 
 
     }
---
+++ app/src/main/java/com/ayushm/movielist/adapter/MovieAdapter.java
@@ -0,0 +1,67 @@
+package com.ayushm.movielist.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.ayushm.movielist.R;
+import com.ayushm.movielist.model.MoviesList;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.PicassoProvider;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private List<MoviesList> moviesLists;
+
+    public MovieAdapter(List<MoviesList> moviesLists1){this.moviesLists = moviesLists1;}
+
+
+    @NonNull
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_design,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapter.ViewHolder holder, int position) {
+        String resource = moviesLists.get(position).getStreamIcon();
+    String name = moviesLists.get(position).getName();
+    Double rating = moviesLists.get(position).getRating();
+
+        if (resource.isEmpty()) {
+            holder.imgs.setImageResource(R.drawable.download);
+        } else{
+            Picasso.get().load(moviesLists.get(position).getStreamIcon()).placeholder(R.drawable.download).into(holder.imgs);
+        }
+
+        holder.movieName.setText(name);
+  holder.movieRating.setText(rating.toString());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesLists.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView movieName,movieRating;
+        ImageView imgs;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            movieName = itemView.findViewById(R.id.movie_name);
+            movieRating = itemView.findViewById(R.id.movie_rating);
+            imgs = itemView.findViewById(R.id.roundedImageView);
+
+        }
+    }
+
+}
